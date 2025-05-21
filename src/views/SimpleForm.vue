402e1632c411bacd55e8563e9d823cf1490a1d38
Margarita Khodzka <Margarita.Khodzka@meredith.com>
2025-05-21 11:18:47 -0400

    submitting forms

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="sendForm">
 
       <BaseSelect
         :options="categories"
@@ -61,6 +61,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
@@ -88,6 +90,20 @@ export default {
         { label: 'Yes', value: 1 },
         { label: 'No', value: 0 }
       ]
+    }
+  },
+  methods: {
+    sendForm () {
+      axios.post(
+        'https://my-json-server.typicode.com/Code-Pop/Vue-3-Forms/events',
+        this.event
+      )
+        .then(function (response) {
+          console.log('Response', response)
+        })
+        .catch(function (err) {
+          console.log('Error', err)
+        })
     }
   }
 }
